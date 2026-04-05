d208fc6a30ac42ff926f622aa2b97d02809f2466
NguyenQuocKhanh <raycrop555@gmail.com>
2026-04-05 18:24:21 +0700

    Update Excercise 3-4-2026

--- 3-4-2026/Excercise.js
+++ 3-4-2026/Excercise.js
@@ -1,116 +1,139 @@
 let books = [];
-let users = [];
 let nextId = 1;
-
-class Book {
-    constructor(id, title, author, quantity) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.quantity = quantity;
-    }
-
-    lend() {
-        if (this.quantity > 0)
-            this.quantity--;
-    }
-
-    return() {
-        this.quantity++;
-    }
-}
-
-function SearchBook(title) {
-    let result = [];
-
-    for (let book of books) {
-        if (book.title === title) {
-            result.push(book);
-        }
-    }
-
-    return result;
-}
-
-function AddBook(title, author, quantity) {
-    if (!title || !author) {
-        console.log("Nhap thieu thong tin, vui long nhap lai");
-        return;
-    }
-
-    for (let book of books) {
-        if (book.title === title && book.author === author) {
-            book.quantity += quantity;
-            return;
-        }
-    }
-
-    const newBook = new Book(nextId, title, author, quantity);
-    nextId++;
-    books.push(newBook);
-}
-
-function DeleteBook(title) {
-    if (!title) {
-        console.log("Vui long nhap ten sach");
-        return;
-    }
-
-    for (let i = 0; i < books.length; i++) {
-        if (books[i].title === title) {
-            books.splice(i, 1);
-            console.log("Xoa thanh cong");
-            return;
-        }
-    }
-
-    console.log("Ten sach khong ton tai");
-}
-
-function AddUser(name, phoneNumber, address) {
-    if (!name || !phoneNumber || !address) {
-        console.log("Nhap thieu thong tin vui long nhap lai")
-    }
-
-    const newUser = {
-        id: nextId,
-        name: name,
-        phoneNumber: phoneNumber,
-        address: address 
-    };
-
-    users.push(newUser);
-}
 
 const readline = require("readline");
 const r1 = readline.createInterface( {
     input: process.stdin,
     output: process.stdout
-})
+});
 
-function ShowMenu() {
-    console.clear();
-    console.log("Chon tinh nang ma ban muon");
-    console.log("1. Xem danh sach sach hien co");
-    console.log("2. Them sach")
-    console.log("3. Sua thong tin sach")
-    console.log("4. Xoa thong tin sach")
+const fs = require("fs");
+const FILE_NAME = "books.json";
 
-    r1.question("Chon: ", HandleMenu)
+function LoadData() {
+    if (fs.existsSync(FILE_NAME)) {
+        const data = fs.readFileSync(FILE_NAME, "utf8");
+        books = JSON.parse(data);
+
+        if (books.length > 0) {
+            nextId = Math.max(...books.map(b => b.id)) + 1;
+        }
+    }
 }
 
+function SaveData() {
+    fs.writeFileSync(FILE_NAME, JSON.stringify(books, null, 2));
+}
+
+function ShowBooks() {
+    console.log("Danh sách sách hiện có");
+
+    if (books.length === 0) {
+        console.log("Hiện không có sách nào");
+    }
+    else {
+        books.forEach(b => {
+            console.log(`ID: ${b.id}. ${b.title} - ${b.author}`);
+        });
+    }
+
+    ShowMenu();
+}
+
+function AddBook() {
+    r1.question("Nhập tên sách: ", (title) => {
+        r1.question("Nhập tên tác giả: ", (author) => {
+            const book = {
+                id: nextId++,
+                title,
+                author
+            };
+            
+            books.push(book);
+            SaveData();
+            console.log("Thêm thành công");
+            ShowMenu();
+        });
+    });
+}
+
+function UpdateBook() {
+    r1.question("Nhập ID sách bạn muốn sửa: ", (id) => {
+        id = parseInt(id);
+
+        const book = books.find(b => b.id === id);
+
+        if (!book) {
+            console.log("Sách bạn muốn sửa không tồn tại");
+            return ShowMenu();
+        }
+
+        r1.question("Nhập tên sách mới: ", (title) => {
+            r1.question("Nhập tên tác giả mới: ", (author) => {
+                book.title = title;
+                book.author = author;
+
+                SaveData();
+                console.log("Cập nhật thành công");
+                ShowMenu();
+            });
+        });
+    });
+}
+
+function DeleteBook(title) {
+    r1.question("Nhập ID sách bạn muốn xóa: ", (id) => {
+        id = parseInt(id);
+
+        const index = books.findIndex(b => b.id === id);
+
+        if (index === -1) {
+            console.log("Sách bạn muốn xóa không tồn tại")
+        }
+        else {
+            books.splice(index, 1);
+            SaveData();
+            console.log("Xóa thành công");
+        }
+
+        ShowMenu();
+    });
+}
+
+function ShowMenu() {
+    console.log("Chọn tính năng mà bạn muốn");
+    console.log("1. Hiển thị danh sách sách hiện có");
+    console.log("2. Thêm sách")
+    console.log("3. Sửa thông tin sách")
+    console.log("4. Xóa sách")
+    console.log("0. Thoát")
+
+    r1.question("Chọn: ", HandleMenu)
+}
 
 function HandleMenu(choice) {
     switch(choice) {
         case "1":
+            ShowBooks();
             break;
         case "2":
+            AddBook();
             break;
         case "3":
+            UpdateBook();
             break;
         case "4":
+            DeleteBook();
             break;
+        case "0":
+            console.log("Thoát thành công");
+            r1.close();
+            break
         default:
+            console.log("Lựa chọn của bạn không hợp lệ");
             ShowMenu();
     }
 }
+
+LoadData();
+ShowMenu();
